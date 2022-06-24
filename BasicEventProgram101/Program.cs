34574fb9fd19eec74f628edca9a476fd31085f63
KgaugeloEliasMatlou <eliasmatlou66@gmail.com>
2022-06-24 09:24:36 +0200

    Add project files.

---
+++ BasicEventProgram101/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+public class EvtPublisher
+{
+    public delegate void del(string x);
+    public event del evt;
+
+    public void checkbalance(int x)
+    {
+
+    }
+}
+
+class program
+{
+    static void Main()
+    {
+
+    }
+}
